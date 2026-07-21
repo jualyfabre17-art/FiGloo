c7f1760be15f3ec1aabb616bef32d4133af6bc5c
Jualy <jualyfabre17@gmail.com>
2026-07-21 16:24:07 -0400

    Terminando de desarrollar las nominas

---
+++ FiGloo/Gerente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FiGloo
+{
+    public class Gerente : EmpleadoTiempoCompleto, IEvaluable
+    {
+        private double bonoPorEquipo;
+        private int cantidadIntegrantes;
+
+        public Gerente(double salario, double bonoPorEquipo, int cantidadIntegrantes ) : base(salario)
+        {
+            this.bonoPorEquipo = bonoPorEquipo;
+            this.cantidadIntegrantes = cantidadIntegrantes;
+        }
+
+        public override double CalcularPago()
+        {
+            return base.CalcularPago() + bonoPorEquipo;
+        }
+
+        public override void MostrarInfo()
+        {
+            Empleado baseRef = this;     
+            baseRef.GetType();           
+            Console.WriteLine($" {nombre} — Gerente");
+            Console.WriteLine($"  Pago: ${CalcularPago():F2}  ({(CalcularPago() - bonoPorEquipo):F2} + {bonoPorEquipo:F2})");
+            Console.WriteLine($"  Equipo: {cantidadIntegrantes} personas");
+            
+        }
+
+        public string Evaluar ()
+        {
+            if (cantidadIntegrantes >= 5)
+                return $"Excelente — equipo de {cantidadIntegrantes} personas bien gestionado.";
+            else if (cantidadIntegrantes >= 2)
+                return $"Bueno — equipo de {cantidadIntegrantes} personas en crecimiento.";
+            else
+                return $"Regular — equipo muy pequeño ({cantidadIntegrantes} persona).";
+        }
+    }
+}
---
+++ FiGloo/Program.cs
@@ -0,0 +1,33 @@
+﻿using FiGloo;
+
+
+class SistemaNomina{
+
+    static void Main()
+    {
+        List<Empleado> empleados = new List<Empleado>();
+
+        while (true) {
+            Console.WriteLine("1.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
